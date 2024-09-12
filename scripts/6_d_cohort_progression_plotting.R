fc547436fbd93935f643010799d3f38aeca61a43
Donal Ring <DonalRing@tfl.gov.uk>
2024-09-12 16:52:19 +0100

    More plotting of the cohorts over time.

--- scripts/6_d_cohort_progression_plotting.R
+++ scripts/6_d_cohort_progression_plotting.R
@@ -5,6 +5,14 @@
 ## 0. libraries and functions
 library(data.table)
 
+functions_to_read <- list.files("functions")
+
+lapply(
+  paste0("functions/", functions_to_read),
+  FUN = source
+)
+
+
 
 ## 1. reading in the datasets
 
@@ -19,11 +27,7 @@ colnames(pupils)[colnames(pupils) == "itl221cd"] <- "itl22cd"
 
 
   ### 1.2. reading in projections
-reception_projections <- fread("output_projections/initial_tenyear/reception_projections_2023_2032_ratio_ets.csv")
-
-year_one_projections <- fread("output_projections/initial_tenyear/year_one_projections_2023_2032_ratio_ets.csv")
-
-year_two_eleven_forecasts <- fread("output_projections/initial_tenyear/year2_year11_projections.csv")
+projections <- fread("output_projections/initial_tenyear/reception_year_11_projections_2023_2032.csv")
 
   ### 1.3. reading in the "data resource" of the inputs for plotting
 plotting_inputs <- fread("data_resources/inputs_for_plotting_cohort_tracking.csv")
@@ -36,19 +40,12 @@ colnames(pupils)[colnames(pupils) == "headcount"] <- "pupil_number"
 
 pupils <- pupils[, c("year", "itl22cd", "nc_year", "pupil_number")]
 
-reception_projections[, nc_year := "reception"]
-colnames(reception_projections)[colnames(reception_projections) == "mean_projection"] <- "pupil_number"
-reception_projections <- reception_projections[, c("year", "itl22cd", "nc_year", "pupil_number")]
+colnames(projections)[colnames(projections) == "mean_projection"] <- "pupil_number"
 
-year_one_projections[, nc_year := "year_group_1"]
-colnames(year_one_projections)[colnames(year_one_projections) == "mean_projection"] <- "pupil_number"
-year_one_projections <- year_one_projections[, c("year", "itl22cd", "nc_year", "pupil_number")]
-
-year_two_eleven_forecasts <- year_two_eleven_forecasts[, c("year", "itl22cd", "nc_year", "pupil_number")]
-
+projections <- projections[, c("year", "itl22cd", "nc_year", "pupil_number")]
 
   ### 2.2. rbinding them all into one, and adding itl names
-pupil_data <- rbind(pupils, reception_projections, year_one_projections, year_two_eleven_forecasts)
+pupil_data <- rbind(pupils, projections)
 
 itl_code_name_lookup <- fread("lookups/itl_code_name_lookup.csv")
 
@@ -61,81 +58,17 @@ pupil_data[itl22nm == "Gloucestershire, Wiltshire and Bath/Bristol area",
            itl22nm :=  "Gloucestershire, Wiltshire and Bath&Bristol area"] # need to change the name of this itl, because when I try to create a filename using the itl name, the slash messes it up
 
 
-## 3. 
-
-### 3.1. defining and testing the function to extract the plot data
-
-unique_geogs <- pupil_data[, unique(itl22cd)]
-geog <- unique_geogs[1]
-
-start_cohort_year <- 2018
-start_cohort_nc_year <- "reception"
-end_cohort_nc_year <- "year_group_11"
-pupil_data <- pupil_data
-max_year <- 2032
-
-extract_cohort_data <- function(start_cohort_year, start_cohort_nc_year, end_cohort_nc_year, geog, pupil_data, max_year = 2032){
-  
-  ### getting the sequence of nc years that we need to project for
-  nc_year_vec <- c("reception", "year_group_1", "year_group_2", "year_group_3", "year_group_4", "year_group_5", "year_group_6", "year_group_7", 
-                   "year_group_8", "year_group_9", "year_group_10", "year_group_11", "year_group_12", "year_group_13", "year_group_14")
-  
-  nc_year_inds <- (which(nc_year_vec == start_cohort_nc_year)):(which(nc_year_vec == end_cohort_nc_year))
-  
-  nc_year_seq <- nc_year_vec[nc_year_inds]
-  
-  ### getting the sequence of calendar years that we need to project for
-  year_seq <- (start_cohort_year):(start_cohort_year + length(nc_year_seq) - 1)
-  
-  ### getting rid of any years included in the sequence above that go beyond the maximum year in the dataset
-  year_seq <- year_seq[year_seq <= max_year]
-  
-  nc_year_seq <- nc_year_seq[1:length(year_seq)]
-  
-  ### extracting the data for the calendar year:nc year combinations
-  cohort_tracking_ind <- numeric(length(nc_year_seq))
-  
-  plot_data <- data.table(
-    year = year_seq,
-    nc_year = nc_year_seq,
-    pupil_number = numeric(length(year_seq))
-  )
-
-  for(i in 1:nrow(plot_data)){
-    
-    ind <- which(pupil_data$year == year_seq[i] & pupil_data$nc_year == nc_year_seq[i] & pupil_data$itl22cd == geog)
-    
-    plot_data[i, "pupil_number"] <- pupil_data[ind, "pupil_number"]
-    
-    
-  }
-  
-  return(plot_data)
-
-  
-}
-
-check <- extract_cohort_data(start_cohort_year = 2018, start_cohort_nc_year = "reception", end_cohort_nc_year = "year_group_11", geog = unique_geogs[1], pupil_data = pupil_data)
-
-geog <- unique_geogs[1]
-
+## 3. making a plot of cohort progression throughout the years
 
   ### 3.2. making the plot - tracking progression through the cohorts - all of them for one geography, on one graph
+unique_geogs <- pupil_data[, unique(itl22cd)]
 
 years_to_plot <- c(2015, 2017, 2019, 2021)
 line_cols <- c("purple", "darkgreen", "orange", "darkblue")
 
-j <- 1
-
 for(j in 1:length(unique_geogs)){
   
   geog <- unique_geogs[j]
-  
-  #ylim_max <- pupil_data[itl22cd == geog, 
-  #                       max(pupil_number)]
-  
-  #ylim_min <- pupil_data[itl22cd == geog & year == 2032 & nc_year == "year_group_11",
-  #                       min(pupil_number)]
   
   all_pupils_in_tracking <- list()
   
@@ -177,6 +110,82 @@ for(j in 1:length(unique_geogs)){
     lines(x = plot_data[, year], y = plot_data[, pupil_number], lwd = 2, col = line_col)
   }
   
+  dev.off()
+
+}
+
+
+## 4. making a similar set of plots, but this time making the x-axis reception, year 1, year 2, etc, and indexing to the first year, to see if the trends are different
+
+unique_geogs <- pupil_data[, unique(itl22cd)]
+
+years_to_plot <- c(2015, 2017, 2019, 2021)
+line_cols <- c("purple", "darkgreen", "orange", "darkblue")
+
+geog <- unique_geogs[j]
+
+for(j in 1:length(unique_geogs)){
+
+  geog <- unique_geogs[j]
+
+    #### extracting the data for the cohorts that we want. 
+  all_pupils_in_tracking <- list()
+
+  for(i in 1:length(years_to_plot)){
+  
+    start_cohort_year <- years_to_plot[i]
+  
+    dat <- extract_cohort_data(start_cohort_year = start_cohort_year, start_cohort_nc_year = "reception", end_cohort_nc_year = "year_group_11", geog = geog, pupil_data = pupil_data)
+  
+    all_pupils_in_tracking[[i]] <- dat
+  
+  }
+
+    #### turning them into indices
+  all_pupils_in_tracking_ind <- lapply(
+    X = all_pupils_in_tracking,
+    FUN = function(data){return(data[, pn_index := 100*(pupil_number/pupil_number[1])])}
+  )
+
+
+    #### getting the y-axis limits and the plot name (which will be the name of the ITL2 geography)
+  ylim_max <- max(unlist(lapply(
+    X = all_pupils_in_tracking_ind,
+    FUN = function(dt){return(max(dt[, pn_index]))}
+  )))
+
+  ylim_min <- min(unlist(lapply(
+    X = all_pupils_in_tracking_ind,
+    FUN = function(dt){return(min(dt[, pn_index]))}
+  )))
+
+  plot_name <- pupil_data[itl22cd == geog, unique(itl22nm)]
+
+    #### making the plot
+  png(file = paste0("plots/cohort_tracking_indexed/", plot_name, ".png"),
+      height = 7, width = 12, units = "in", res = 600)
+  
+  plot(x = 1:12, y = rep(1, 12), type = "n", bty = "n", axes = FALSE,
+      ylim = c(ylim_min, ylim_max), ylab = "", xlab = "")
+
+  axis(side = 2, las = 1)
+  axis(side = 1, at = 1:12, 
+      labels = c("R", "Year 1", "Year 2", "Year 3", "Year 4", "Year 5", "Year 6", "Year 7", "Year 8", "Year 9", "Year 10", "Year 11"),
+      las = 2)
+
+  title(main = plot_name)
+
+  for(i in 1:length(all_pupils_in_tracking_ind)){
+  
+    series_to_plot <- all_pupils_in_tracking_ind[[i]][, pn_index]
+  
+    lines(x = 1:12, y = series_to_plot, lwd = 3, col = line_cols[i])
+  
+  }
+
+  legend("topright", legend = c("2015 start", "2017 start", "2019 start", "2021 start"), 
+        col = line_cols, lwd = 3, bty = "n")
+
   dev.off()
 
 }
